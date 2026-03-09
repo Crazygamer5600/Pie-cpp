cf18b96d6e1401c0cb5648622372b8e8e9ba132e
MinaGirgis-AdelaideUni <a1924861@student.adelaide.edu.au>
2026-03-09 16:37:18 +1100

    Add project files.

---
+++ pie.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+class Pie {
+	public:
+	std::string describer;
+	std::string tastiness;
+
+	Pie(std::string desc, std::string taste) {
+		describer = desc;
+		tastiness = taste;
+	}
+	virtual std::string description() = 0 {
+		return describer;
+	}
+};
+
+class ApplePie : public Pie {
+	public:
+
+	ApplePie(std::string desc, std::string taste) : Pie(desc, taste) {};
+
+	std::string description() {
+		return describer;
+	}
+
+	void summary() {
+		std::cout << describer << " " << tastiness;
+	}
+};
+
+int main() {
+
+
+	ApplePie pie("yummy!", "Delicious!");
+	pie.summary();
+
+
+	return 0;
+}
